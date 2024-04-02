8caea090d8931e1ba06f3302b35d7ae20ef1f04b
domxcobb <domxcobb@gmail.com>
2024-04-02 00:54:17 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, useLocation } from "react-router-dom";
+
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from './components/Contact';
+
 
 function App() {
+  const location = useLocation();
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <Routes location={location} key={location.pathname}>
+        <Route index element={<Home />} />
+        <Route path={"/about"} element={<About />} />
+        <Route path={"/contact"} element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
